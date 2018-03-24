bb3723296712537c4022ee2a2ddadf5d790b3d5f
Esdras Rios Baia <esdrasrioos@gmail.com>
2018-03-24 00:45:36 -0300

    fix test details

--- index.spec.js
+++ index.spec.js
@@ -14,11 +14,11 @@ describe('test', () => {
     const result = solution([0, 3, 3, 11, 5, 3, 7, 1]);
     expect(result).toEqual(4);
   });
-  it('return maxAdj in the arr (expected -2)', () => {
+  it('return -2 given a arr with no adjValues (expected -2)', () => {
     const result = solution([1]);
     expect(result).toEqual(-2);
   });
-  it('return maxAdj in the arr (expected -2)', () => {
+  it('return maxAdj in the arr given a negative arr(expected 5)', () => {
     const result = solution([0, -3, -3, -12, -5, -3, -7, -1]);
     expect(result).toEqual(5);
   });
